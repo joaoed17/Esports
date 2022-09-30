a37817e7c22de2cd902c6e6bf75992ffd31fbda6
Murilo-V <alveiramurilo@hotmail.com>
2022-09-30 13:25:49 -0300

    fix: servers

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/games/", async (req, res) => {
+  // O CÓDIGO ABAIXO CRIA UM GAME NO BANCO, DESCOMENTE ESSA LINHA PARA CRIAR UM GAME E RETORNAR O ARRAY DE GAMES
+  // await prisma.games.create({
+  //   data: {
+  //     name: 'tesste',
+  //     bannerUrl: 'https://i.pinimg.com/originals/f8/f3/01/f8f301698392ee89abd583fe98c83a54.jpg',
+  //   },
+  // })
   const game = await prisma.games.findMany();
   return res.json(game);
 });
